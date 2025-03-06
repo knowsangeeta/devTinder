ab10712f15394ab095b8fabc12a879a450fbf2ed
knowsangeeta <ksange0301@gmail.com>
2025-03-06 14:54:30 +0530

    git, put, post, delete, use api

--- src/app.js
+++ src/app.js
@@ -3,9 +3,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
+    res.send('Hello Geet!');
+});
+app.get('/user', (req, res) => {
+    res.send({ firstname: 'Geet', lastname: 'S' });
+});
+app.post('/user', (req, res) => {
+    res.send('user added successfully');
+});
+app.delete('/user', (req, res) => {
+    res.send('deleted successfully');
+});
+app.use('/test', (req, res) => {
     res.send('Hello World!');
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
