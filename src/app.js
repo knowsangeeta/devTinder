6387bc1fb79c1b8ea755b75b84e40d29ed7d341b
knowsangeeta <ksange0301@gmail.com>
2025-03-10 19:26:08 +0530

    routing and request handlers

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.get('/hello', (req, res) => {
-    res.send('Hello Geet!');
-});
-app.get('/user', (req, res) => {
+app.get('/user/:userId/:name/:password', (req, res) => {
+    console.log(req.params);
     res.send({ firstname: 'Geet', lastname: 'S' });
 });
-app.post('/user', (req, res) => {
-    res.send('user added successfully');
-});
-app.delete('/user', (req, res) => {
-    res.send('deleted successfully');
-});
-app.use('/test', (req, res) => {
-    res.send('Hello World!');
-});
-
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
