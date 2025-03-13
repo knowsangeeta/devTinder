df33190fe367db7013fbc62a2cf29888c0a09034
knowsangeeta <ksange0301@gmail.com>
2025-03-13 17:41:35 +0530

    Middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 
 const app = express();
-const port = 3001;
+const port = 7777;
 
-app.get('/user/:userId/:name/:password', (req, res) => {
-    console.log(req.params);
-    res.send({ firstname: 'Geet', lastname: 'S' });
-});
+app.get("/user",
+    (req, res, next) => {
+        console.log("Handling the route user!!!");
+        next();
+
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 2");
+        next();
+
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 3");
+        next();
+
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 3");
+        res.send("5th response");
+
+    }
+);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
