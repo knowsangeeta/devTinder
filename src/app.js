e2059d561834bef1c94636650103b678497c3143
knowsangeeta <ksange0301@gmail.com>
2025-04-12 18:34:14 +0530

    diving into apis

--- src/app.js
+++ src/app.js
@@ -19,6 +19,75 @@ app.post("/signup", async(req, res) => {
     });
 });
 
+app.get("/user", async(req, res) => {
+    const userEmail = req.body.email;
+    try{
+        const user = await User.find({ email: userEmail });
+        if(user.length === 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.send(user);
+    } catch(err) {
+        console.error("Error fetching user", err);
+        res.status(400).json({ message: "Error fetching user" });
+    }
+
+})
+app.get("/feed", async(req, res) => {
+    const userEmail = req.body.email;
+    try{
+        const user = await User.find({});
+        if(user.length === 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.send(user);
+    } catch(err) {
+        console.error("Error fetching user", err);
+        res.status(400).json({ message: "Error fetching user" });
+    }
+
+})
+app.delete("/user", async(req, res) => {
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete({_id: userId});
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.send("User deleted successfully");
+    } catch(err) {
+        console.error("Error fetching user", err);
+        res.status(400).json({ message: "Error fetching user" });
+    }
+
+})
+app.delete("/userbyEmail", async(req, res) => {
+    const userEmail = req.body.email;
+    try{
+        const user = await User.findOneAndDelete({email:userEmail});
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.send("User deleted successfully");
+    } catch(err) {
+        console.error("Error fetching user", err);
+        res.status(400).json({ message: "Error fetching user" });
+    }
+
+})
+app.patch("/user", async(req, res) => {
+    const data   = req.body;
+    const userId = req.body.userId;
+
+    try{
+        const user = await User.findByIdAndUpdate({_id: userId},data,{returnDocument: 'before'});
+        console.log(user);
+        res.send("User updated successfully");
+    }catch(err){
+        
+        res.status(400).json({ message: "Error updating user" });
+    }
+})
 connectDB().then(() => {
     console.log("Connected to MongoDB Atlas");
     app.listen(port, () => {
