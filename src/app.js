2cc70501a7159d843ae02dc6515fbd873dc7ccaa
knowsangeeta <ksange0301@gmail.com>
2025-04-15 13:06:22 +0530

    login API

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.post("/signup", async (req, res) => {
     try {
         // Validation of data
         validateSignUpData(req);
-        const { firstName, lastName, emailId, password } = req.body;
+        const { firstName, lastName, email, password } = req.body;
         // Encrypt the password
         const passwordHash = await bcrypt.hash(password, 10);
         console.log(passwordHash);
@@ -31,7 +31,25 @@ app.post("/signup", async (req, res) => {
         res.status(400).send("ERROR : " + err.message);
     }
 });
-
+app.post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        throw new Error("Invalid credentials");
+      }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if (isPasswordValid) {
+        res.send("Login Successful!!!");
+      } else {
+        throw new Error("Invalid credentials");
+      }
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  });
 app.get("/user", async (req, res) => {
     const userEmail = req.body.email;
     try {
