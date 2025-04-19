4db2d831aaf6a56451559fc103312381e9943596
knowsangeeta <ksange0301@gmail.com>
2025-04-19 21:00:20 +0530

    .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect(
-        "mongodb+srv://ksange0301:oaSh0k9wn0MDoJmZ@cluster0.se6glrp.mongodb.net/devTinder"
-    );
-}
+    await mongoose.connect(process.env.MONGODB_URI);
+};
+
 
 
 module.exports = connectDB;
